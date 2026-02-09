b83e8183181027e83c9efb67ea0d74076d3ecbee
Mate Technologies <rogermodu@gmail.com>
2026-02-09 14:43:16 +0700

    Update devices.js

--- utils/devices.js
+++ utils/devices.js
@@ -1,12 +1,5 @@
 module.exports = [
-  {
-    name: "desktop",
-    width: 1366,
-    height: 768
-  },
-  {
-    name: "mobile",
-    width: 375,
-    height: 812
-  }
+  { name: "desktop", width: 1366, height: 768 },
+  { name: "tablet",  width: 1024, height: 768 },
+  { name: "mobile",  width: 375,  height: 812 }
 ];
